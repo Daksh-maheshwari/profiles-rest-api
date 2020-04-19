db12a3c7d28a306145a529272d32abfe1772d8be
Daksh <daksh.k.maheshwari@gmail.com>
2020-04-19 22:58:44 +0530

    added POST, PUT, PATCH, DELETE ADDED

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,9 +1,14 @@
 from rest_framework.views import APIVIEW
 from rest_framework.response import Response
+from rest_framework import status
+
+from profiles_api import serializers
 
 
 class HelloApiView(APIVIEW):
     """Test API View"""
+    serializer_class=serializers.HelloSerializer
+
 
     def get(self, request, format=None):
         """Returns a list of APIView feature"""
@@ -15,3 +20,27 @@ class HelloApiView(APIVIEW):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview })
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializeer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """DELETE an OBject """
+        return Response({'method': 'DELETE'})
